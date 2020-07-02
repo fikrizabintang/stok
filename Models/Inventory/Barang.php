26691068c800f9d5180c8747e6cff5c923c8ffbe
Fikriza Bintang <fikrizabintang@gmail.com>
2020-07-03 00:51:10 +0700

    Update Barang.php

--- Models/Inventory/Barang.php
+++ Models/Inventory/Barang.php
@@ -9,6 +9,6 @@ class Barang extends Model
     protected $table = 'dc_barang';
 
     public function stok(){
-        return $this->hasMany('App\Models\Inventory\Stok','id_barang')->selectRaw('id_barang, sum(masuk)')->where('id_unit', Auth::user()->id_unit)->groupBy('id_barang');
+      return $this->hasMany('App\Models\Inventory\Stok','id_barang')->selectRaw('id_barang, IFNULL(SUM(masuk) - SUM(keluar), 0) AS stok')->where('id_unit', Auth::user()->id_unit)->groupBy('id_barang');
     }
 }
