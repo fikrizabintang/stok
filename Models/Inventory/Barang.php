ad3ffa14c9b0969d612794c4507bb274880e361c
Fikriza Bintang <fikrizabintang@gmail.com>
2020-07-02 23:26:19 +0700

    Update Barang.php

--- Models/Inventory/Barang.php
+++ Models/Inventory/Barang.php
@@ -7,16 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $table = 'dc_barang';
-
-    public function pabrik(){
-          return $this->belongsTo('App\Models\MasterData\Inventory\Pabrik','id_pabrik');
-    }
-
-    public function stok(){
-          return $this->hasMany('App\Models\Inventory\Stok','id_barang');
-    }
-
-    public function sisa_stok(){
-          return $this->hasMany('App\Models\Inventory\Stok','id_barang');
-    }
 }
