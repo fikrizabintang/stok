59ca0912beb522e9d5be7cefa5458dface0fbfd6
Fikriza Bintang <fikrizabintang@gmail.com>
2020-07-02 23:26:40 +0700

    Update Stok.php

--- Models/Inventory/Stok.php
+++ Models/Inventory/Stok.php
@@ -7,16 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Stok extends Model
 {
     protected $table = 'dc_stok';
-
-    public function barang(){
-          return $this->belongsTo('App\Models\Inventory\Barang','id_barang');
-      }
-
-    public function kemasan_barang(){
-          return $this->belongsTo('App\Models\Inventory\KemasanBarang','id_barang','id_barang');
-      }
-
-    public function users(){
-          return $this->belongsTo('App\User','id_users');
-      }
 }
