a3cded2a040fce3185a74e356c5c803d2a42d89c
Fikriza Bintang <fikrizabintang@gmail.com>
2020-07-03 00:48:54 +0700

    Update Barang.php

--- Models/Inventory/Barang.php
+++ Models/Inventory/Barang.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $table = 'dc_barang';
+
+    public function stok(){
+        return $this->hasMany('App\Models\Inventory\Stok','id_barang')->selectRaw('id_barang, sum(masuk)')->where('id_unit', Auth::user()->id_unit)->groupBy('id_barang');
+    }
 }
