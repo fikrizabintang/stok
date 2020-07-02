21da7a560492b0a273e403fc662c61501ca01fe2
Fikriza Bintang <fikrizabintang@gmail.com>
2020-07-02 23:24:32 +0700

    Update API_BarangController.php

--- Http/Controllers/Api/API_BarangController.php
+++ Http/Controllers/Api/API_BarangController.php
@@ -6,55 +6,43 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Inventory\Barang;
 use App\Models\Inventory\Stok;
+use App\Repositories\Eloquent\InventoryRepository;
 
-use Carbon\Carbon;
-
-class API_BarangController extends Controller
+class API_ObatController extends Controller
 {
+    protected $inventoryRepositories;
+
+    public function __construct(InventoryRepository $inventoryRepositories)
+    {
+        $this->inventoryRepositories = $inventoryRepositories;
+    }
 
     public function index(Request $request)
     {
-        // if(request()->ajax()){
           $cari = $request->q;
-          $now = Carbon::now();
+          $barang = Barang::where('nama', 'like', "$cari%")
+          ->where('aktif','Ya')
+          ->paginate(15);
 
-          $supplier = Barang::
-          where('aktif', 'Ya')
-          ->where('nama', 'like', "$cari%")
-          ->orderBy('id', 'asc');
-
-          if ($request->ekatalog)
-             $supplier = $supplier->where('ekatalog', $request->ekatalog);
-          if (isset($request->golongan) && $request->golongan <> 1)
-             $supplier = $supplier->where('id_golongan', $request->golongan);
-          if ($request->kategori)
-             $supplier = $supplier->where('id_kategori', $request->kategori);
-
-          $supplier = $supplier->paginate(5);
-
-          $supplier_count = Barang::limit(5);
+          $barang_count = Barang::limit(15);
 
           $data = [
-              'total'           => $supplier_count->count(),
+              'total'           => $barang_count->count(),
           ];
 
-          foreach($supplier as $key => $post) {
-              $harga_satuan_supplier = ($post->hpp) - ($post->hpp * $post->diskon/100 );
-              $sisa_stok    = Stok::selectRaw('IFNULL(SUM(masuk) - SUM(keluar), 0) AS stok')
-              ->where('id_barang', $post->id)
-              ->where('id_unit', 40)
-              ->where('ed', '>', $now)->first();
+          foreach($barang as $key => $post) {
+              $harga_rajal  = ( (($post->margin_resep_rajal / 100) * $post->hpp) + $post->hpp - 0.01);
+              $sisa_stok    = $this->inventoryRepositories->sisaStok($post->id);
 
               $data['data'][$key] = [
-                  'id'                => $post->id,
-                  'nama_lengkap'      => $post->nama_lengkap,
-                  'harga_satuan'      => $harga_satuan_supplier,
-                  'stok'              => $sisa_stok,
+                  'id'              => $post->id,
+                  'nama'            => $post->nama_lengkap,
+                  'harga'           => number_format((float) round($harga_rajal, 2), 2, ',', '.'),
+                  'id_sediaan'      => $post->id_sediaan,
+                  'stok'            => $sisa_stok,
               ];
           }
           return response()->json($data);
-        // }
-        //   return view('error.index');
     }
 
 }
