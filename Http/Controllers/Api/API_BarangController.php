37b8e7428aefab54500a0ed09aa5116e76f88718
Fikriza Bintang <fikrizabintang@gmail.com>
2020-07-25 13:56:44 +0700

    Update API_BarangController.php

--- Http/Controllers/Api/API_BarangController.php
+++ Http/Controllers/Api/API_BarangController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Inventory\Barang;
-use App\Models\Inventory\Stok;
 use App\Repositories\Eloquent\InventoryRepository;
 
-class API_ObatController extends Controller
+class ObatController extends Controller
 {
     protected $inventoryRepositories;
 
@@ -19,28 +17,7 @@ class API_ObatController extends Controller
 
     public function index(Request $request)
     {
-          $cari = $request->q;
-          $barang = Barang::where('nama', 'like', "$cari%")
-          ->where('aktif','Ya')
-          ->paginate(15);
-
-          $barang_count = Barang::limit(15);
-
-          $data = [
-              'total'           => $barang_count->count(),
-          ];
-
-          foreach($barang as $key => $post) {
-              $sisa_stok    = $this->inventoryRepositories->sisaStok($post->id);
-
-              $data['data'][$key] = [
-                  'id'              => $post->id,
-                  'nama'            => $post->nama_lengkap,
-                  'id_sediaan'      => $post->id_sediaan,
-                  'stok'            => $sisa_stok,
-              ];
-          }
-          return response()->json($data);
+        return $this->inventoryRepositories->sisaStok();
     }
 
 }
